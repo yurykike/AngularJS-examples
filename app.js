cec9390b2df0838582d4c89c221d3a6bffdeee78
Enrique Navarro Goicoechea <enriquenavarrogoicoechea@MacBook-Air-de-Enrique.local>
2016-06-29 13:14:07 +0200

    Usando listas con angular

--- app.js
+++ app.js
@@ -3,6 +3,30 @@
 console.log("hola!");
 var myApp = angular.module('myApp', []);
 
-myApp.controller('MyController', function($scope) {
+myApp.controller('MyController', function ($scope) {
     $scope.message = "Hello, its me";
+    $scope.people = [
+        {
+            name: "Peter"
+            , age: 20
+        }
+        , {
+            name: "Jeny"
+            , age: 25
+        }
+        , {
+            name: "George"
+            , age: 15
+        }
+    ];
+    
+    $scope.addPerson = function() {
+        console.log("boton pulsado!");
+        
+        var person = {
+            name : $scope.name,
+            age : $scope.age
+        };
+        $scope.people.push(person);
+    };
 });
